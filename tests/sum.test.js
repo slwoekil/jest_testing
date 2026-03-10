bec2b34cbe362b9c1a61a7e8fa84551381f13205
ayo-eyo <art.danilov95@gmail.com>
2026-03-10 21:01:20 +0200

    Простые тесты сложения

---
+++ tests/sum.test.js
@@ -0,0 +1,13 @@
+const sum = require('../app/sum.js')
+
+test('1 + 2 = 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('0 + 0 = 0', () => {
+    expect(sum(0, 0)).toBe(0);
+});
+
+test('-1 + -2 = -3', () => {
+    expect(sum(-1, -2)).toBe(-3);
+});
